af860ff09894341d1f9528d4cf0bc98a7755f577
NameLess, the Jedi <yoddah@gmail.com>
2011-09-23 13:33:10 +0200

    Missing import

--- services/java/com/android/server/status/widget/SleepButton.java
+++ services/java/com/android/server/status/widget/SleepButton.java
@@ -5,6 +5,7 @@ import com.android.server.status.widget.PowerButton;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.provider.Settings;
 import android.os.PowerManager;
 import android.os.SystemClock;
